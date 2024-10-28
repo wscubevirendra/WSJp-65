c39faa9c0992fbab345fbc14b8ea6769f76543f0
wscubevirendra <virendrarathore@wscubetech.com>
2024-10-28 17:03:50 +0530

    Adv js

--- Media query/day 02/js/app.js
+++ Media query/day 02/js/app.js
@@ -15,7 +15,7 @@ bars.addEventListener(
 
 navbarOverlay.addEventListener(
     'click',
-    function(){
+    function () {
         navbar.classList.remove('open');
         navbarOverlay.classList.remove('open-navbar-overlay');
 
@@ -23,3 +23,52 @@ navbarOverlay.addEventListener(
     }
 )
 
+document.addEventListener(
+    'scroll',
+    function (e) {
+
+        if (window.scrollY > 48) {
+            document.body.classList.add('stick')
+        }else{
+            document.body.classList.remove('stick')
+
+        }
+    }
+)
+
+
+$('.multiple-items').slick({
+    dots: true,
+    infinite: false,
+    speed: 5000,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
